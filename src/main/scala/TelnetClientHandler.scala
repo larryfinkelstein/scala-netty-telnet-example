6987252d64a9e4ae042b110dc18ce8e522272732
Larry Finkelstein <larry.finkelstein@bms.com>
2016-12-21 00:06:02 -0500

    Add logging

--- src/main/scala/TelnetClientHandler.scala
+++ src/main/scala/TelnetClientHandler.scala
@@ -1,3 +1,8 @@
+import java.net.InetAddress
+import java.util.Date
+import java.util.concurrent.SynchronousQueue
+
+import com.typesafe.scalalogging.Logger
 import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
 
 /**
@@ -6,9 +11,32 @@ import io.netty.channel.{ChannelHandlerContext, SimpleChannelInboundHandler}
   * Handles a client-side channel.
   *
   */
+object TelnetClientHandler {
+  var msgs = new SynchronousQueue[String]
+}
+
 class TelnetClientHandler extends SimpleChannelInboundHandler[String] {
+  import TelnetClientHandler.msgs
+
+  val log = Logger("TelnetClientHandler")
+
+  override def channelActive(ctx: ChannelHandlerContext): Unit = {
+    log info "channel active"
+    // Send greeting for a new connection.
+//    ctx.write("Welcome to " + InetAddress.getLocalHost().getHostName() + "!\r\n");
+//    ctx.write("It is " + new Date() + " now.\r\n");
+//    ctx.flush();
+    super.channelActive(ctx)
+  }
+
   override def channelRead0(ctx: ChannelHandlerContext, msg: String): Unit = {
-    println(msg)
+    log info s"Received $msg"
+//    msgs.add(msg)
+  }
+
+  override def channelRead(ctx: ChannelHandlerContext, msg: AnyRef): Unit = {
+    log info msg.toString
+    super.channelRead(ctx, msg)
   }
 
   override def channelReadComplete(ctx: ChannelHandlerContext): Unit = {
