a278dbb8056d1f796528c891cd4a5373cb9d6589
Larry Finkelstein <larry.finkelstein@bms.com>
2016-12-21 00:05:00 -0500

    Add logging ... small restructuring

--- src/main/scala/TelnetClient.scala
+++ src/main/scala/TelnetClient.scala
@@ -1,9 +1,8 @@
-import java.io.{BufferedReader, InputStreamReader}
+import java.io.{BufferedReader, IOException, InputStreamReader}
 
+import com.typesafe.scalalogging.Logger
 import io.netty.bootstrap.Bootstrap
-import io.netty.channel.Channel
-import io.netty.channel.ChannelFuture
-import io.netty.channel.EventLoopGroup
+import io.netty.channel.{AbstractChannel, Channel, ChannelFuture, EventLoopGroup}
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.nio.NioSocketChannel
 
@@ -16,58 +15,67 @@ import scala.util.control.Breaks.{break, breakable}
   * Simplistic telnet client.
   */
 object TelnetClient {
+  val log = Logger("TelnetClient")
   def main(args: Array[String]) {
     // Print usage if no argument is specified.
     if (args.length != 2) {
-      System.err.println(
-        "Usage: " + TelnetClient.getClass.getSimpleName +
-          " <host> <port>")
-      return
-    }
+      System.err.println(s"Usage: ${TelnetClient.getClass.getSimpleName} <host> <port>")
+    } else {
+      // Parse options.
+      val host = args(0)
+      val port = args(1).toInt
 
-    // Parse options.
-    val host = args(0)
-    val port = args(1).toInt
+      val group: EventLoopGroup = new NioEventLoopGroup()
 
-    val group: EventLoopGroup = new NioEventLoopGroup()
+      try {
+        val b: Bootstrap = new Bootstrap()
+        b.group(group)
+          .channel(classOf[NioSocketChannel])
+          .handler(new TelnetClientInitializer)
 
-    try {
-      val b: Bootstrap = new Bootstrap()
-      b.group(group)
-        .channel(classOf[NioSocketChannel])
-        .handler(new TelnetClientInitializer)
+        // Start the connection attempt.
+        val ch: Channel = b.connect(host, port).sync().channel()
 
-      // Start the connection attempt.
-      val ch: Channel = b.connect(host, port).sync().channel()
+        // Read commands from the stdin.
+        var lastWriteFuture: ChannelFuture = null
+        val in: BufferedReader = new BufferedReader(new InputStreamReader(System.in))
 
-      // Read commands from the stdin.
-      var lastWriteFuture: ChannelFuture = null
-      val in: BufferedReader = new BufferedReader(new InputStreamReader(System.in))
+        //TODO rewrite the loop as a recursive function. (Refer to Programming in Scala, 7.6 Living without break and continue)
+        breakable {
+          while (true) {
+            val line = in.readLine
+            if (line == null) break
 
-      //TODO rewrite the loop as a recursive function. (Refer to Programming in Scala, 7.6 Living without break and continue)
-      breakable {
-        while (true) {
-          val line = in.readLine
-          if (line == null) break
+            // Sends the received line to the server.
+            val start = System.currentTimeMillis()
+            log info s"Sending $line"
+            lastWriteFuture = ch.writeAndFlush(line + "\r\n")
+//            log info s"${System.nanoTime() - start}")
+            lastWriteFuture.await()
+            log info s"${System.currentTimeMillis() - start}"
 
-          // Sends the received line to the server.
-          lastWriteFuture = ch.writeAndFlush(line + "\r\n")
-
-          // If user typed the 'bye' command, wait until the server closes
-          // the connection.
-          if ("bye".equals(line.toLowerCase())) {
-            ch.closeFuture().sync()
-            break
+            // If user typed the 'bye' command, wait until the server closes
+            // the connection.
+            if ("bye".equals(line.toLowerCase())) {
+              ch.closeFuture().sync()
+              break
+            }
           }
         }
-      }
 
-      // Wait until all messages are flushed before closing the channel.
-      if (lastWriteFuture != null) {
-        lastWriteFuture.sync()
+        // Wait until all messages are flushed before closing the channel.
+        if (lastWriteFuture != null) {
+          lastWriteFuture.sync()
+        }
+      } catch {
+        case e: IOException =>
+          log error (e.getMessage)
+        case e: Exception =>
+          log error(e.getMessage, e)
+      } finally {
+        group.shutdownGracefully()
       }
-    } finally {
-      group.shutdownGracefully()
     }
+
   }
 }
